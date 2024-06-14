8d74c28dc7f1ba093d9ba5a10e4a9ba26a2702d7
MaybeMonad <dezineleo@gmail.com>
2024-06-14 18:09:14 +0800

    update: daruma assets

--- src/components/live-preview.tsx
+++ src/components/live-preview.tsx
@@ -1,4 +1,5 @@
 import { useVGG } from "@verygoodgraphics/vgg-react"
+import { useEffect, useState } from "react"
 
 function AnimatedLoadingDots() {
   return (
@@ -17,24 +18,49 @@ export function PreviewVGG() {
     // verbose: true,
     // editMode: true,
   })
+  const [height, setHeight] = useState(window.innerHeight)
+  const [showTips, setShowTips] = useState(false)
+
+  useEffect(() => {
+    const appHeight = () => {
+      const doc = document.documentElement
+      doc.style.setProperty("--app-height", `${window.innerHeight}px`)
+      setHeight(window.innerHeight)
+    }
+    window.addEventListener("resize", appHeight)
+    appHeight()
+  }, [])
+
+  useEffect(() => {
+    setTimeout(() => {
+      if (isRendered) return
+      setShowTips(true)
+    }, 10 * 1000)
+  }, [])
 
   return (
     <>
       {!isRendered && (
         <div className="fixed left-0 top-0 z-[9999] h-screen w-screen bg-black">
-          <div className="flex h-screen w-screen items-center justify-center bg-black text-white/80">
+          <div className="flex h-screen w-screen items-center flex-col justify-center bg-black text-white/80">
             <AnimatedLoadingDots />
+            {showTips && (
+              <p className="text-xs mt-4 text-white/70">
+                Downloading resources... If this persists, please check your
+                network.
+              </p>
+            )}
           </div>
         </div>
       )}
-      <div className="relative flex min-h-screen items-center justify-center overflow-hidden">
+      <div className="relative flex min-h-screen items-start justify-center overflow-hidden">
         <canvas
           ref={canvasRef}
           style={{
             width: "100vw",
-            height: "100vh",
+            height: height,
           }}
-          className="bg-white outline-none"
+          className="bg-white outline-none h-[var(--app-height)]"
         />
       </div>
     </>
